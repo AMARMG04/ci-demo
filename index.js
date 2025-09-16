c064bb70d260b4714abc7c5fce249c31e0dce877
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2025-09-16 21:20:57 +0530

    Fixed the jest

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.json({ status: "ok" }));
-app.get("/health", (req, res) => res.send("healthy"));
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening ${port}`));
-
-module.exports = app; // for tests
