d94cf75994cedbcf493f87e43bf3d7132b1da61e
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2025-09-17 17:57:09 +0530

    Added changes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.json({ status: "ok" }));
-app.get("/health", (req, res) => res.send("healthy"));
+app.get("/health", (req, res) => res.send("healthy as fuck"));
 
 module.exports = app;
